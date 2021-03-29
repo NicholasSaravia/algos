794447a3b94ef26d5542eab9c1f83d1220e8c14e
Nick Saravia <nick.saravia@gmail.com>
2021-03-28 23:01:54 -0400

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,27 @@
+function selectionSort(array) {
+  let lengthOfArray = array.length;
+
+  for (let i = 0; i < lengthOfArray; i++) {
+    // set smallest number in array
+    let smallestNumIndex = i;
+
+    // start another loop entire array starting at position of smallest num index + 1
+    for (let j = i + 1; j < lengthOfArray; j++) {
+      if (array[j] < array[smallestNumIndex]) {
+        // update smallest num index
+        smallestNumIndex = j;
+      }
+    }
+
+    // check if smallest number changed based of index
+    if (smallestNumIndex != i) {
+      // swap places
+      let tmp = array[i];
+      array[i] = array[smallestNumIndex];
+      array[smallestNumIndex] = tmp;
+    }
+  }
+  return array;
+}
+
+console.log(selectionSort([9, 15, 2, 44, -1, 36, 1]));
